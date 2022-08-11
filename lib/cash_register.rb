1c5438ae9d573b950bbc8e5e16aadf559f9b1dbd
Agnes <agsawicka@gmail.com>
2022-08-11 12:03:38 -0400

    commit tree 233dc58c1e69cb22889efc0ccd9d1831cf135c3f

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+class CashRegister
+    def initialize(total= 0, *employee_discount)
+        @total = total
+        @employee_discount = employee_discount
+    end
 
+    def total
+        @total
+    end
+
+    def add_item
+    end
+
+end
